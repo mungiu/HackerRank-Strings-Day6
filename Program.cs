a5de0d72b1012f81d7de39b93669f8a1bcb9c173
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-08 19:11:36 +0200

    Add files via upload
    I have added my progress on the application to this point.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /*
+        int T = Convert.ToInt32(Console.ReadLine());
+        int N = S.Length;
+        */
+        
+        string S = Console.ReadLine();
+        char[] charS = S.ToCharArray();
+
+        string evenIndexed = string.Empty;
+        string oddIndexed = string.Empty;
+        StringBuilder eI = new StringBuilder();
+        StringBuilder oI = new StringBuilder();
+        
+        int x = 0;
+        foreach (char j in charS)
+        {
+            if (x % 2 == 0)
+            {
+                eI.Append(j);
+            }
+
+            else
+            {
+                oI.Append(j);
+            }
+            x += 1;
+        }
+        evenIndexed = eI.ToString();
+        oddIndexed = oI.ToString();
+
+        Console.WriteLine(evenIndexed);
+        Console.WriteLine(oddIndexed);
+    }
+}
