1048202f9da659c09b3b94e9aabf377208db6d6b
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-08 20:16:11 +0200

    Update Program.cs
    I have finally managed to take the process out in two methods, however the output is still not correct. Though it is close to what i require.
    
    I believe the next step will be to create out of the two just one method which will take 2 inputs and give two outputs at the same time.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -7,22 +7,32 @@ class Solution
 {
     static void Main(String[] args)
     {
-        string S = Console.ReadLine();
-        char[] charS = S.ToCharArray();
+        Console.WriteLine("Input T: ");
+        int T = Convert.ToInt32(Console.ReadLine());
+
+        //i need to read 2 strings and perform teh same operation on both of them
+        Console.WriteLine(FirstIndexing());
+        Console.WriteLine(SecondIndexing());
+    }
+
+    static string FirstIndexing()
+    {
+        Console.WriteLine("Input 1: ");
+        string fI = Console.ReadLine();
+        char[] charfI= fI.ToCharArray();
 
         string evenIndexed = string.Empty;
         string oddIndexed = string.Empty;
         StringBuilder eI = new StringBuilder();
         StringBuilder oI = new StringBuilder();
-        
+
         int x = 0;
-        foreach (char j in charS)
+        foreach (char j in charfI)
         {
             if (x % 2 == 0)
             {
                 eI.Append(j);
             }
-
             else
             {
                 oI.Append(j);
@@ -32,7 +42,36 @@ class Solution
         evenIndexed = eI.ToString();
         oddIndexed = oI.ToString();
 
-        Console.WriteLine(evenIndexed);
-        Console.WriteLine(oddIndexed);
+        return ($"{evenIndexed}  {oddIndexed}");
+    }
+
+    static string SecondIndexing()
+    {
+        Console.WriteLine("Input 2: ");
+        string sI = Console.ReadLine();
+        char[] charsI = sI.ToCharArray();
+
+        string evenIndexed = string.Empty;
+        string oddIndexed = string.Empty;
+        StringBuilder eI = new StringBuilder();
+        StringBuilder oI = new StringBuilder();
+
+        int x = 0;
+        foreach (char j in charsI)
+        {
+            if (x % 2 == 0)
+            {
+                eI.Append(j);
+            }
+            else
+            {
+                oI.Append(j);
+            }
+            x += 1;
+        }
+        evenIndexed = eI.ToString();
+        oddIndexed = oI.ToString();
+
+        return ($"{evenIndexed}  {oddIndexed}");
     }
 }
