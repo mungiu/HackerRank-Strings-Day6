45288180cc75ad32d699d8b27bbf66d76e6a170a
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-07-09 10:00:11 +0200

    Update Program.cs - Solved
    Now all outputs match the requirements.
    User can input T number of times.

--- Program.cs
+++ Program.cs
@@ -9,29 +9,24 @@ class Solution
     {
         Console.WriteLine("Input T: ");
         int T = Convert.ToInt32(Console.ReadLine());
-
-        //i need to read 2 strings and perform teh same operation on both of them
-        Indexing();
+        
+        while (T > 0)
+        {
+            Indexing();
+        }
     }
 
     static void Indexing()
     {
-        Console.WriteLine("Input 1: ");
+        Console.WriteLine("Input: ");
         string first = Console.ReadLine();
-        Console.WriteLine("Input 2: ");
-        string second = Console.ReadLine();
-
         char[] charFirst= first.ToCharArray();
-        char[] charSecond = second.ToCharArray();
-
         string evenIndexFirst = string.Empty;
         string oddIndexFirst = string.Empty;
-        string evenIndexSecond = string.Empty;
-        string oddIndexSecond = string.Empty;
-        
         StringBuilder eF = new StringBuilder();
         StringBuilder oF = new StringBuilder();
         int x = 0;
+
         foreach (char j in charFirst)
         {
             if (x % 2 == 0)
@@ -46,26 +41,6 @@ class Solution
         }
         evenIndexFirst = eF.ToString();
         oddIndexFirst = oF.ToString();
-
-        StringBuilder eS = new StringBuilder();
-        StringBuilder oS = new StringBuilder();
-        int y = 0;
-        foreach (char j in charSecond)
-        {
-            if (y % 2 == 0)
-            {
-                eS.Append(j);
-            }
-            else
-            {
-                oS.Append(j);
-            }
-            y += 1;
-        }
-        evenIndexSecond = eS.ToString();
-        oddIndexSecond = oS.ToString();
-
-        Console.WriteLine($"{evenIndexFirst} {oddIndexFirst}" +
-            $"\n{ evenIndexSecond} {oddIndexSecond}");
+        Console.WriteLine($"{evenIndexFirst} {oddIndexFirst}");
     }
 }
